ac509fb0d1b540645fc46d55d7cad8d46f9cd186
Dylan Kay <dylan.kay@me.com>
2017-05-03 15:28:32 +1000

    Mobile Site
    Added settings page and theme functionality

---
+++ mobile/mobile-code.js
@@ -0,0 +1,10 @@
+theme = getThemeFromStorage();
+
+if (theme) {
+	let elem = document.createElement("link");
+	elem.setAttribute("rel", "stylesheet");
+	elem.setAttribute("href", "../codemirror-5.24.2/theme/" + theme + ".css");
+	document.getElementsByTagName("head")[0].appendChild(elem);
+	
+	editor.setOption("theme", theme);
+}
